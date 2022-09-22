4b7785810085392687c008352542e74154428fbc
Sukhdeep Singh <sukhdeep.singh1@outlook.com>
2022-09-22 12:38:59 +0200

    Initial setup

---
+++ sandbox/main.tf
@@ -0,0 +1,44 @@
+locals {
+  env = "sit"
+  default_tags = {
+    env                = local.env
+    systemNumber       = "12345"
+    billingDescription = "Sandbox"
+    billingContact     = "sukhdeep.singh@posten.no"
+    billingUnit        = "123456"
+    creationSource     = "terraform"
+  }
+  region      = "norwayeast"
+  instance_id = "1"
+  rg-common   = "${local.env}-rg-prodapp-common"
+  rg-infra    = "${local.env}-rg-prodapp-infra"
+}
+
+terraform {
+  backend "azurerm" {
+    subscription_id      = "18c8d488-9b69-4619-ab58-724f279fe48e" // ProdApp-55493-DEV
+    resource_group_name  = "terraform-rg"
+    storage_account_name = "btfsandbox12"
+    container_name       = "tfstate"
+    key                  = "sandbox/sandbox.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.20.0"
+    }
+  }
+  required_version = "1.2.8"
+}
+
+provider "azurerm" {
+  subscription_id = "18c8d488-9b69-4619-ab58-724f279fe48e" // ProdApp Dev Subscription
+  features {}
+}
+
+data "azurerm_client_config" "current" {
+}
+
+data "azurerm_resource_group" "sandbox" {
+  name = "${local.env}-rg-sandbox"
+}
